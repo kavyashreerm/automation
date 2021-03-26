91f4b9871ed5db31a6db3f4f9393c5932fcbaca8
kavyashreerm <kavyashreerm1@gmail.com>
2021-03-26 19:58:11 +0530

    even numbers

---
+++ src/programs/even.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int no=10;
+			for(int i=1;i<=no;i++)
+			{
+				if(i%2==0)
+				{
+					System.out.println(i+"is"+"even number");
+				}
+				else {
+					System.out.println(i+"is"+"not a even number");
+				}
+			}
+	}
+
+}
